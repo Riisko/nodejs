3639a2dd550e28dba1a60bdc734067b449686342
Boris <rir@seznam.cz>
2017-04-23 09:59:26 +0000

    Start of full stack course

---
+++ testing and debugging/test/order-spec.js
@@ -0,0 +1,29 @@
+var expect = require("chai").expect;
+var rewire = require("rewire");
+var order = rewire("../lib/order");
+var sinon = require("sinon");
+
+describe("Ordering Items", function() {
+    
+    beforeEach(function() {
+        this.testData = [
+            {sku:"AAA", qty: 10},
+            {sku:"BBB", qty: 0},
+            {sku: "CCC", qty: 3}
+                
+            ];
+            this.console = {
+                log: sinon.spy()
+            };
+        order.__set__("inventoryData", this.testData);
+        order.__set__("console", this.console);
+    });
+    
+    it("order an item when there are enough in stock", function(done) {
+        var _this = this;
+        order.orderItem("CCC", 3, function() {
+            expect(_this.console.log.callCount).to.equal(1);
+            done();
+        });
+    });
+});
--- testing and debugging/test/tools-spec.js
+++ testing and debugging/test/tools-spec.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools")
+var nock = require("nock");
 
 describe("Tools", function() {
     describe("printName()", function() {
@@ -11,10 +12,16 @@ describe("Tools", function() {
         });
     });
     describe("loadWiki()", function() {
+        
+        before(function() {
+            nock("https://en.wikipedia.org")
+                .get("/wiki/Abraham_Lincoln")
+                .reply(200, "Mock Abraham Lincoln Page");
+        });
        
        it("Load Abraham Lincoln's wiki page", function (done) {
           tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
-              expect(html).to.be.ok;
+              expect(html).to.equal("Mock Abraham Lincoln Page");
               done();
           });
        });
