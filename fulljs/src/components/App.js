900a862fd480314c116e12174563294c17113dd1
Boris <rir@seznam.cz>
2017-04-23 22:08:52 +0000

    testData

---
+++ fulljs/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          <ContestPreview {...this.props.contest[0]} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
